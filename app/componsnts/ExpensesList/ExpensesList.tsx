a2242dcca9aab9c952a3ea71c80cf5e3c766f379
AdamKniec <adam.k.kniec@gmail.com>
2026-04-21 20:12:30 +0200

    adding dummy data

--- app/componsnts/ExpensesList/ExpensesList.tsx
+++ app/componsnts/ExpensesList/ExpensesList.tsx
@@ -1,7 +1,14 @@
 import { FlatList, Text } from "react-native"
 
+const DATA = [
+    {id: '1', name: 'Grocery', price: 12, date: '01.01.2026'},
+    {id: '2', name: 'Water', price: 122, date: '02.01.2026'},
+    {id: '3', name: 'Taxes', price: 1, date: '03.01.2026'},
+    {id: '4', name: 'Light', price: 132, date: '04.01.2026'},
+    {id: '5', name: 'Restaurant', price: 125, date: '05.01.2026'},
+]
 export const ExpensesList = () => {
-    return <FlatList data={[]} renderItem={item => <Text>1</Text>} />
+    return <FlatList data={DATA} renderItem={({item}) => <Text>{item.name}</Text>} />
 }
 
 export default ExpensesList
